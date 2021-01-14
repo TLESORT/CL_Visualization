f0200d42d03214ad22009de8b3a6da946677713b
TLESORT <t.lesort@gmail.com>
2021-01-14 17:19:47 -0500

    update

--- ewc_diag.py
+++ ewc_diag.py
@@ -16,36 +16,38 @@ class EWC_Diag(Trainer):
         super().__init__(scenario, continuum, model)
         self.model = model
         self.layer_collection = LayerCollection.from_model(model)
-        
+
         self.importance = 100
         self.list_Fishers = {}
         self.algo_name = "ewc_diag"
 
     def compute_fisher(self, task_set, model, ind_task):
+        fisher_set = deepcopy(task_set)
 
-		fisher_set = deepcopy(task_set)
-        
         F_diag = FIM(model=model,
-                                 loader=self.fisher_set,
-                                 representation=PMatDiag,
-                                 device='cuda')
+                     loader=fisher_set,
+                     representation=PMatDiag,
+                     device='cuda')
 
         v0 = PVector.from_model(model).clone().detach()
 
         return F_diag, v0
 
-    def init_task(self, ind_task):
-        pass
 
-    def callback_task(self, ind_task):
-        self.list_Fishers[ind_task] = self.compute_fisher(self.model, ind_task)
+def init_task(self, ind_task):
+    pass
 
-    def regularize_loss(self, model, loss):
-        v = PVector.from_model(model)
 
-        for i, (fim, v0) in self.list_Fishers.items():
-            loss += self.importance * fim.vTMv(v - v0)
+def callback_task(self, ind_task):
+    self.list_Fishers[ind_task] = self.compute_fisher(self.model, ind_task)
 
-        assert loss == loss  # sanity check to detect nan
 
-        return loss
+def regularize_loss(self, model, loss):
+    v = PVector.from_model(model)
+
+    for i, (fim, v0) in self.list_Fishers.items():
+        loss += self.importance * fim.vTMv(v - v0)
+
+    assert loss == loss  # sanity check to detect nan
+
+    return loss
