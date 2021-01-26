5c2e96af71bb250f99a3608cf761737d4d538c46
timothee.lesort <timothee.lesort@login-3.server.mila.quebec>
2021-01-26 18:09:22 -0500

    a taskset class designed for rehearsal strategies

---
+++ memory.py
@@ -0,0 +1,22 @@
+
+
+
+from continuum import TaskSet
+
+
+class MemorySet(TaskSet):
+    """
+    A task set designed for Rehearsal strategies
+    """
+
+    """
+    To concatenate two tasksets
+    """
+    def concatenate(self):
+        pass
+
+    """
+    artificially increase size of memory for balance purpose
+    """
+    def increase_size(self):
+        pass
