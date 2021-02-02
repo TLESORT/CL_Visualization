c0f2e6245eed7bc203fbf2b28c690ba7cba54d53
TLESORT <t.lesort@gmail.com>
2021-02-02 12:44:39 -0500

    reorganization of memory management

--- rehearsal.py
+++ rehearsal.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.nn import functional as F
 from copy import deepcopy
 from torch.utils import data
+import numpy as np
 
 import os
 from copy import copy
@@ -12,31 +13,32 @@ from memory import MemorySet
 
 
 class Rehearsal(Trainer):
-    def __init__(self, root_dir, dataset, scenario_name, model, num_tasks):
-        super().__init__(root_dir, dataset, scenario_name, model, num_tasks)
+    def __init__(self, root_dir, dataset, scenario_name, model, num_tasks, dev):
+        super().__init__(root_dir, dataset, scenario_name, model, num_tasks, dev)
         self.algo_name = "rehearsal"
         self.data_memory = None
         self.num_classes_per_task = 2
-        self.nb_samples_rehearsal = 100
+        self.nb_samples_rehearsal_per_class = 100
         self.samples_transfer = 5000
         self.sample_num = 100
         self.input_size = 1
         self.image_size = 28
-        self.sample_dir = os.path.join(self.root_dir , "Samples")
+        self.sample_dir = os.path.join(self.root_dir, "Samples")
 
+    def init_task(self, ind_task: int, task_set: TaskSet):
+        # ptit checkup
+        if ind_task > 0:
+            assert len(self.data_memory) == self.data_memory.nb_classes * self.nb_samples_rehearsal_per_class
 
-    def init_task(self, ind_task):
+    def callback_task(self, ind_task: int, task_set: TaskSet):
         if ind_task < self.num_tasks:
-            self.manage_memory(ind_task, self.nb_samples_rehearsal, self.samples_transfer)
+            self.manage_memory(ind_task, task_set)
 
-        self.scenario_tr[ind_task].plot(self.sample_dir, f"training_{ind_task}.png",
-                                      nb_samples= 100,
-                                      shape = [self.image_size, self.image_size, self.input_size])
+        task_set.plot(self.sample_dir, f"training_{ind_task}.png",
+                              nb_samples=100,
+                              shape=[self.image_size, self.image_size, self.input_size])
 
-    def callback_task(self, ind_task):
-        pass
-
-    def manage_memory(self, ind_task, nb_samples_rehearsal, samples_transfer):
+    def manage_memory(self, ind_task: int, task_set: MemorySet):
         """
         Method to select samples for rehearsal
         :param ind_task: index of the task to select samples from
@@ -44,31 +46,41 @@ class Rehearsal(Trainer):
         :param samples_transfer: number of samples to incorporate in the new training set (samples_transfer > nb_samples_rehearsal)
         :return: updated train_set and test_set
         """
-        # save sample before modification of training set
-        self.scenario_tr.set_task(ind_task)
-        x_tr, y_tr = self.scenario_tr.get_sample(nb_samples_rehearsal, shape=[self.input_size, self.image_size, self.image_size])
-        self.task_samples = copy(x_tr).reshape(-1, self.input_size* self.image_size* self.image_size)
-        self.task_labels = copy(y_tr)
+        nb_classes = task_set.nb_classes
+        assert self.nb_samples_rehearsal_per_class * nb_classes < len(task_set._y)
+        indexes = np.random.randint(0, len(task_set._y), self.nb_samples_rehearsal_per_class * nb_classes)
+        samples, labels, task_ids = task_set.get_raw_samples(indexes)
 
-        # create data loader with memory from previous task
-        if ind_task > 0:
-            # put the memory inside the training dataset
-            self.scenario_tr.add_samples(self.data_memory)
-
-        increase_factor = int(samples_transfer / self.nb_samples_rehearsal) * self.num_classes_per_task
-        assert increase_factor > 0
-
-        if ind_task == 0:
-            self.data_memory = MemorySet(self.task_samples, self.task_labels)
-            #self.data_memory.increase_size(increase_factor)
+        if self.data_memory is not None:
+            self.data_memory.add_samples(samples, labels, task_ids)
         else:
-            #new_data = TaskSet(self.task_samples, self.task_label)
-            #new_data.increase_size(increase_factor)
-            self.data_memory.add_samples(self.task_samples, self.task_label)
+            self.data_memory = MemorySet(samples, labels, task_ids, None)
 
         self.data_memory.plot(self.sample_dir, f"memory_{ind_task}.png",
-                                      nb_samples= 100,
-                                      shape = [self.image_size, self.image_size, self.input_size])
+                              nb_samples=100,
+                              shape=[self.image_size, self.image_size, self.input_size])
 
+    def one_task_training(self, ind_task: int, task_set: TaskSet):
+        """
+        In vanilla rehearsal we add memory data to training data and balance classes
+        """
 
+        # We convert task set to a memory set because we need to manipulate the list_ID to balance classes
+        task_memory_set = None
+        if ind_task > 0:
+            task_memory_set = MemorySet(task_set._x,
+                                        task_set._y,
+                                        task_set._t,
+                                        task_set.trsf,
+                                        task_set.data_type)
 
+            task_memory_set.concatenate(self.data_memory)
+            task_memory_set.balance_classes()
+
+            print("############  INFOS #############")
+            print(task_memory_set.get_classes())
+            print(len(task_memory_set))
+        else:
+            task_memory_set = task_set
+
+        super().one_task_training(ind_task, task_memory_set)
