117894d794ea8803e2906e2bde5a9cee7e0c0248
TLESORT <t.lesort@gmail.com>
2021-02-05 11:14:39 -0500

    new test for concatenation and fix bugs

--- tests/test_memory_set.py
+++ tests/test_memory_set.py
@@ -48,18 +48,60 @@ def test_increase_memory(increase_factor):
 
     memory_set.increase_size(increase_factor=increase_factor)
 
-    assert len(memory_set) == int(before_len * increase_factor)
+    assert len(memory_set) == int(round(before_len * increase_factor))
     # the number of samples stay the same only the id list grows
     assert memory_set.get_nb_samples() == before_nb_samples
 
 
-def test_concatenate():
+def test_simple_concatenate():
     (x_1, y_1, t_1), (x_2, y_2, t_2) = gen_data()
 
     memory_set_1 = MemorySet(x_1, y_1, t_1, None)
     memory_set_2 = MemorySet(x_2, y_2, t_2, None)
 
+    classes_1 = memory_set_1.get_classes()
+    classes_2 = memory_set_2.get_classes()
+
+    len_1 = len(memory_set_1)
+    len_2 = len(memory_set_2)
+
     memory_set_1.concatenate(other_memory_set=memory_set_2)
+
+    assert len(memory_set_1) == len_1 + len_2
+
+    cat_classes = memory_set_1.get_classes()
+    for _class in (list(classes_1) + list(classes_2)):
+        assert _class in cat_classes, f"{cat_classes} vs {classes_1} + {classes_2}"
+
+
+@pytest.mark.parametrize("increase_factors", [[1.6, 1], [1.5, 2.5], [1, 3], [1.75, 2.25]])
+def test_concatenate_with_size_increase(increase_factors):
+    (x_1, y_1, t_1), (x_2, y_2, t_2) = gen_data()
+    fact1, fact2 = increase_factors
+
+    memory_set_1 = MemorySet(x_1, y_1, t_1, None)
+    memory_set_2 = MemorySet(x_2, y_2, t_2, None)
+
+    if fact1 > 1.0: memory_set_1.increase_size(increase_factor=fact1)
+    if fact2 > 1.0: memory_set_2.increase_size(increase_factor=fact2)
+
+    classes_1 = memory_set_1.get_classes()
+    classes_2 = memory_set_2.get_classes()
+
+    instances_1 = memory_set_1.get_nb_samples()
+    instances_2 = memory_set_2.get_nb_samples()
+
+    len_1 = len(memory_set_1)
+    len_2 = len(memory_set_2)
+
+    memory_set_1.concatenate(other_memory_set=memory_set_2)
+
+    assert len(memory_set_1) == len_1 + len_2
+    assert memory_set_1.get_nb_samples() == instances_1 + instances_2
+
+    cat_classes = memory_set_1.get_classes()
+    for _class in (list(classes_1) + list(classes_2)):
+        assert _class in cat_classes, f"{cat_classes} vs {classes_1} + {classes_2}"
 
 
 def test_loader():
@@ -83,7 +125,7 @@ def test_loader_and_increase_factor(increase_factor):
     assert len(train_loader.dataset) == SIZE_MEMORY * increase_factor
 
 
-@pytest.mark.parametrize("increase_factor", [3, 3.5, 1.75])
+@pytest.mark.parametrize("increase_factor", [3, 2.25, 3.5, 1.75])
 def test_memory_increase_size_class(increase_factor):
     (x_1, y_1, t_1), _ = gen_data()
 
@@ -100,9 +142,9 @@ def test_memory_increase_size_class(increase_factor):
 
     new_nb_instances = memory_set.get_nb_instances_class(class_label=class_label)
 
-    assert new_nb_instances == int(nb_initial_instances * increase_factor)
+    assert new_nb_instances == int(round(nb_initial_instances * increase_factor))
     # the number of instance has been multiplied, not the number of samples
-    assert new_nb_instances == int(memory_set.get_nb_samples_class(class_label=class_label) * increase_factor)
+    assert new_nb_instances == int(round(memory_set.get_nb_samples_class(class_label=class_label) * increase_factor))
 
 
 def test_memory_get_nb_samples_class():
@@ -129,6 +171,6 @@ def test_memory_classes_balance():
     nb_instances = 0
     for i, _class in enumerate(classes):
         if i != 0:
-            assert nb_instances == memory_set.get_nb_instances_class(_class),\
+            assert nb_instances == memory_set.get_nb_instances_class(_class), \
                 f"index {i}, class {_class} : {nb_instances} vs {memory_set.get_nb_instances_class(_class)}"
         nb_instances = memory_set.get_nb_instances_class(_class)
