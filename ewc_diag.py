6d41317a2e7f551a509493386de0f1fb47389a24
timothee.lesort <timothee.lesort@kepler4.server.mila.quebec>
2021-02-05 11:12:14 -0500

    small fix for ewc

--- ewc_diag.py
+++ ewc_diag.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from copy import deepcopy
-from torch.utils import data
+from torch.utils.data import DataLoader
 
 from nngeometry.metrics import FIM
 from nngeometry.layercollection import LayerCollection
@@ -11,21 +11,9 @@ from nngeometry.object.vector import PVector
 from trainer import Trainer
 
 
-class EWC_Diag(Trainer):
-    def __init__(self, root_dir, dataset, scenario_name, model, num_tasks, dev):
-        super().__init__(root_dir, dataset, scenario_name, model, num_tasks, PMatDiag, dev)
-        self.algo_name = "ewc_diag"
-
-
-class EWC_KFAC(Trainer):
-    def __init__(self, root_dir, dataset, scenario_name, model, num_tasks, dev):
-        super().__init__(root_dir, dataset, scenario_name, model, num_tasks, PMatKFAC, dev)
-        self.algo_name = "ewc_kfac"
-
-
 class EWC(Trainer):
     def __init__(self, root_dir, dataset, scenario_name, model, num_tasks, representation, dev):
-        super().__init__(root_dir, dataset, scenario_name, model, num_tasks)
+        super().__init__(root_dir, dataset, scenario_name, model, num_tasks, dev)
         self.model = model
         self.layer_collection = LayerCollection.from_model(model)
         self.representation = representation
@@ -34,23 +22,25 @@ class EWC(Trainer):
         self.list_Fishers = {}
         self.representation = representation
 
-    def compute_fisher(self, task_set, model, ind_task):
+    def compute_fisher(self, ind_task, task_set, model):
         fisher_set = deepcopy(task_set)
 
         F_diag = FIM(model=model,
-                     loader=fisher_set,
+                     loader=DataLoader(fisher_set),
                      representation=self.representation,
+                     n_output=self.scenario_tr.nb_classes,
+                     variant='classif_logits',
                      device='cuda')
 
         v0 = PVector.from_model(model).clone().detach()
 
         return F_diag, v0
 
-    def init_task(self, ind_task):
-        pass
+    def init_task(self, ind_task, task_set):
+        return task_set
 
-    def callback_task(self, ind_task):
-        self.list_Fishers[ind_task] = self.compute_fisher(self.model, ind_task)
+    def callback_task(self, ind_task, task_set):
+        self.list_Fishers[ind_task] = self.compute_fisher(ind_task, task_set, self.model)
 
     def regularize_loss(self, model, loss):
         v = PVector.from_model(model)
@@ -61,3 +51,15 @@ class EWC(Trainer):
         assert loss == loss  # sanity check to detect nan
 
         return loss
+
+
+class EWC_Diag(EWC):
+    def __init__(self, root_dir, dataset, scenario_name, model, num_tasks, dev):
+        super().__init__(root_dir, dataset, scenario_name, model, num_tasks, PMatDiag, dev)
+        self.algo_name = "ewc_diag"
+
+
+class EWC_KFAC(EWC):
+    def __init__(self, root_dir, dataset, scenario_name, model, num_tasks, dev):
+        super().__init__(root_dir, dataset, scenario_name, model, num_tasks, PMatKFAC, dev)
+        self.algo_name = "ewc_kfac"
