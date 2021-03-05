25e4ef76b6e9f284abc685b296f0d5c2f3982577
TLESORT <t.lesort@gmail.com>
2021-03-05 16:58:28 -0500

    modif for estimation of parameters importance for one only class

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 
 import torch.nn as nn
+import torch
 
 class Model(nn.Module):
     def __init__(self, num_classes=10):
@@ -14,6 +15,11 @@ class Model(nn.Module):
         self.maxpool2 = nn.MaxPool2d(kernel_size=2)
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, self.global_num_classes)
+        self.marginalized_class = None
+
+    def set_marginalized_class(self, marginalized_class):
+        assert marginalized_class < self.global_num_classes
+        self.marginalized_class = marginalized_class
 
     def forward(self, x, latent_vector=False):
         x = x.view(-1, 1, 28, 28)
@@ -24,4 +30,10 @@ class Model(nn.Module):
         x = self.relu(self.fc1(x))
         if not latent_vector:
             x = self.fc2(x)
+        if self.marginalized_class is not None:
+            x_class = x[:, self.marginalized_class]
+            x_others = torch.cat([x[:,:self.marginalized_class],x[:,self.marginalized_class+1:]], axis=1)
+            x_others = torch.max(x_others, axis=1)
+            x = torch.cat([x_class, x_others], axis=1)
+            assert x.shape[1] == 2
         return x
