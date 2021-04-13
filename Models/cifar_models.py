fe8cb3fd04b10045f59c77fdfd18e0a4ff1be0ab
TLESORT <t.lesort@gmail.com>
2021-04-13 14:27:03 -0400

    split model for cifar into feature extractor and output layer

--- Models/cifar_models.py
+++ Models/cifar_models.py
@@ -4,6 +4,7 @@ from torchvision import models
 from Models.resnet import cifar_resnet20
 from Models.model_utils import get_Output_layer, freeze_model
 
+
 class CIFARModel(nn.Module):
 
     def __init__(self, num_classes=10, OutLayer="Linear", pretrained_on=None, finetuning=False):
@@ -13,25 +14,32 @@ class CIFARModel(nn.Module):
         self.finetuning = finetuning
 
         if pretrained_on is not None:
-            self.model = cifar_resnet20(pretrained=pretrained_on)
-            self.model.num_classes = 10  # manual correction
+            model = cifar_resnet20(pretrained=pretrained_on)
+            model.num_classes = 10  # manual correction
             if not finetuning:
                 freeze_model(self.model)
         else:
-            self.model = cifar_resnet20()
+            model = cifar_resnet20()
         latent_dim = self.model.fc.in_features
-        self.model.fc = get_Output_layer(OutLayer, latent_dim, num_classes)
+
+        self.feature_extractor = nn.Sequential(*list(model.children())[:-1])
+        self.last = get_Output_layer(OutLayer, latent_dim, num_classes)
+
+        #
+        # self.last = nn.Linear(list(model.children())[-1][-1].in_features, num_classes)
+        # self.classifier = nn.Sequential(*list(model.children())[-1][:-1])
+        # self.features = nn.Sequential(*list(model.children())[:-1])
 
     def get_last_layer(self):
-        return self.model.fc
+        return self.last
 
     def feature_extractor(self, x):
-        # todo
-        pass
+        x=self.feature_extractor(x)
+        return x
 
     def forward(self, x):
-        #x=x.view(-1,3,32,32)
-        x=self.model(x)
+        # x=x.view(-1,3,32,32)
+        x=self.feature_extractor(x)
         return self.last(x)
 
     def update_head(self, batch, labels):
