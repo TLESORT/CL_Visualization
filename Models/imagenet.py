36182fb1a3c267138dc1dd81f3f3cfe77c278578
TLESORT <t.lesort@gmail.com>
2021-04-19 15:44:14 -0400

    update imagenet model to be compatible with model architecture of the project

--- Models/imagenet.py
+++ Models/imagenet.py
@@ -4,26 +4,42 @@ import torch
 import torch.nn as nn
 from torchvision import models
 
+from Models.Output_Layers.head import NNHead
 
 class ImageNetModel(nn.Module):
 
-    def __init__(self, num_classes=10, pretrained=False, name_model="resnet"):
+    def __init__(self, num_classes=10,
+                 pretrained=False,
+                 name_model="resnet",
+                 OutLayer="Linear",
+                 classes_per_head=None,
+                 method="baseline"):
         super(ImageNetModel, self).__init__()
         self.pretrained = pretrained
         self.name_model = name_model
         self.num_classes = num_classes
+        self.data_encoded = False
         if name_model=="alexnet":
             model = models.alexnet(pretrained=True)
         else:
             raise Exception("Ca va pas la")
 
-        self.last = nn.Linear(list(model.children())[-1][-1].in_features, num_classes)
+        self.latent_dim  = list(model.children())[-1][-1].in_features
+
+        self.head = NNHead(input_size=self.latent_dim,
+                           num_classes=self.num_classes,
+                           classes_per_tasks=classes_per_head,
+                           LayerType=OutLayer,
+                           method=method)
+
         self.classifier = nn.Sequential(*list(model.children())[-1][:-1])
         self.features = nn.Sequential(*list(model.children())[:-1])
 
+    def set_data_encoded(self, flag):
+        self.data_encoded = flag
 
     def get_last_layer(self):
-        return self.last
+        return self.head.layer
 
     def feature_extractor(self, x):
         x=self.features(x)
@@ -31,13 +47,15 @@ class ImageNetModel(nn.Module):
         return self.classifier(x)
 
     def forward(self, x):
-        x=x.view(-1,3,224,224)
-        x=self.feature_extractor(x)
-        return self.last(x)
+        if not self.data_encoded:
+            x=x.view(-1,3,224,224)
+            x=self.feature_extractor(x)
+        return self.head(x)
 
     def update_head(self, batch, labels):
         # for SLDA
 
-        batch = self.feature_extractor(batch)
+        if not self.data_encoded:
+            batch = self.feature_extractor(batch)
         self.get_last_layer().update(batch, labels)
 
