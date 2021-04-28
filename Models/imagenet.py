651ba4a6882c96f3d63a2f7b8054543708b94708
TLESORT <t.lesort@gmail.com>
2021-04-28 19:46:35 -0400

    delete model if features are already encoded

--- Models/imagenet.py
+++ Models/imagenet.py
@@ -1,10 +1,9 @@
-
-
 import torch
 import torch.nn as nn
 from torchvision import models
 
 from Models.Output_Layers.head import NNHead
+
 
 class ImageNetModel(nn.Module):
 
@@ -19,12 +18,12 @@ class ImageNetModel(nn.Module):
         self.name_model = name_model
         self.num_classes = num_classes
         self.data_encoded = False
-        if name_model=="alexnet":
+        if name_model == "alexnet":
             model = models.alexnet(pretrained=True)
         else:
             raise Exception("Ca va pas la")
 
-        self.latent_dim  = list(model.children())[-1][-1].in_features
+        self.latent_dim = list(model.children())[-1][-1].in_features
 
         self.head = NNHead(input_size=self.latent_dim,
                            num_classes=self.num_classes,
@@ -37,19 +36,23 @@ class ImageNetModel(nn.Module):
 
     def set_data_encoded(self, flag):
         self.data_encoded = flag
+        if self.data_encoded:
+            # we can free some memory if the data is already encoded
+            self.classifier = None
+            self.features = None
 
     def get_last_layer(self):
         return self.head.layer
 
     def feature_extractor(self, x):
-        x=self.features(x)
-        x=x.view(-1, 9216)
+        x = self.features(x)
+        x = x.view(-1, 9216)
         return self.classifier(x)
 
     def forward(self, x):
         if not self.data_encoded:
-            x=x.view(-1,3,224,224)
-            x=self.feature_extractor(x)
+            x = x.view(-1, 3, 224, 224)
+            x = self.feature_extractor(x)
         x = x.view(-1, self.latent_dim)
         return self.head(x)
 
@@ -60,7 +63,5 @@ class ImageNetModel(nn.Module):
             batch = self.feature_extractor(batch)
         self.get_last_layer().update(batch, labels, epoch)
 
-
     def get_loss(self, out, labels, loss_func, masked=False):
         return self.head.get_loss(out, labels, loss_func, masked)
-
