78420ab301d6d9f1a3071611c8bff9462ee3a04a
TLESORT <t.lesort@gmail.com>
2021-04-28 19:47:27 -0400

    update subset script

--- script/script_subsets.sh
+++ script/script_subsets.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-list_heads="SLDA KNN MeanLayer Linear CosLayer Linear_no_bias"
 list_heads_masked="Linear CosLayer Linear_no_bias MIMO_Linear_no_bias MIMO_Linear MIMO_CosLayer"
+list_heads="SLDA KNN MeanLayer Linear CosLayer Linear_no_bias"
 list_subsets="100 200 500 1000 10000"
 seed=$1
 
 for subset in $list_subsets ;do
 
 for masked_head in $list_heads_masked ;do
-python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR10 --pretrained_on CIFAR100 --OutLayer $masked_head --seed $seed --masked --subset $subset
+#python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR10 --pretrained_on CIFAR100 --OutLayer $masked_head --seed $seed --masked --subset $subset
+python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset Core50 --batch_size 32 --pretrained_on ImageNet --OutLayer $masked_head --seed $seed --masked --subset $subset
 done #list_heads_masked
 
 for head in $list_heads ;do
-python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR10 --pretrained_on CIFAR100 --OutLayer $head --seed $seed --subset $subset
+#python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR10 --pretrained_on CIFAR100 --OutLayer $head --seed $seed --subset $subset
+python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset Core50 --batch_size 32 --pretrained_on ImageNet --OutLayer $head --seed $seed --subset $subset
 done #list_heads
+
 done #list_subsets
+
+
+# python plot.py --scenario_name Disjoint --num_tasks 1  --dataset Core50 --pretrained_on ImageNet --root_dir ../Archives
