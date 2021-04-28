c3697740cca6fbdcaed64871a6c7faf218151ea8
TLESORT <t.lesort@gmail.com>
2021-04-27 22:51:48 -0400

    add subset in results path

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ parser.add_argument('--test_label', action='store_true', default=False,
 parser.add_argument('--masked_out', action='store_true', default=False, help='if true we only update one out dimension')
 parser.add_argument('--subset', type=int, default=None, help='we can replace the full tasks by a subset of samples randomly selected')
 parser.add_argument('--OutLayer', default="Linear", type=str,
-                    choices=['Linear', 'CosLayer', 'SLDA', "Linear_no_bias", 'KNN', 'MIMO_Linear', 'MIMO_Linear_no_bias', 'MIMO_CosLayer', 'MeanLayer'],
+                    choices=['Linear', 'CosLayer', "Linear_no_bias", 'MIMO_Linear', 'MIMO_Linear_no_bias', 'MIMO_CosLayer', 'MeanLayer', 'KNN', 'SLDA'],
                     help='type of ouput layer used for the NN')
 parser.add_argument('--pretrained_on', default=None, type=str,
                     choices=[None, "CIFAR10", "CIFAR100", "ImageNet"],
@@ -55,6 +55,9 @@ if not os.path.exists(config.pmodel_dir):
     os.makedirs(config.pmodel_dir)
 
 config.root_dir = os.path.join(config.root_dir, config.dataset, f"{config.num_tasks}-tasks")
+
+if config.subset is not None:
+    config.root_dir = os.path.join(config.root_dir, f"subset-{config.subset}")
 if config.test_label:
     config.root_dir = os.path.join(config.root_dir, "MultiH")
 else:
