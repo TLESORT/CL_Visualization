5e1198c9b0716ef4d603ca1b69d1afbd9587505d
TLESORT <t.lesort@gmail.com>
2021-05-08 23:25:33 -0400

    individual run to execute

--- script/single_runs.sh
+++ script/single_runs.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-command="python main.py --scenario_name Domain --num_tasks 8 --name_algo baseline  --dataset Core10Lifelong --pretrained_on ImageNet --fast --seed 1664 --OutLayer Linear --architecture resnet"
+
+command="python main.py --scenario_name Disjoint --num_tasks 5 --name_algo baseline --dataset CIFAR10 --pretrained_on CIFAR100 --OutLayer Linear --seed 1 --fast"
 sbatch script/single_on_cluster.sh $command
+
+command="python main.py  --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR100 --pretrained_on CIFAR10 --fast --seed 0 --OutLayer CosLayer"
+sbatch script/single_on_cluster.sh $command
+
+command="python main.py  --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR100 --pretrained_on CIFAR10 --fast --seed 1 --OutLayer Linear --masked_out"
+sbatch script/single_on_cluster.sh $command
+
+
