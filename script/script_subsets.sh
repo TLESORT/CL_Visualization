c90d2192e3013695a8cdb244b4f4f21c8c9fdcc1
TLESORT <t.lesort@gmail.com>
2021-05-09 21:59:45 -0400

    share experiments betweens scripts

--- script/script_subsets.sh
+++ script/script_subsets.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-list_subsets="100 200 500 1000 10000"
 
-for subset in $list_subsets ;do
+python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR10 --pretrained_on CIFAR10 --fast $@
+python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR10 --pretrained_on CIFAR100 --fast $@
+python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR100 --pretrained_on CIFAR10 --fast $@
+python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR100 --pretrained_on CIFAR100 --fast $@
+python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset Core50 --pretrained_on ImageNet --architecture resnet --fast $@
 
-python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR10 --pretrained_on CIFAR10 --fast --subset $subset $@
-python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR10 --pretrained_on CIFAR100 --fast --subset $subset $@
-python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR100 --pretrained_on CIFAR10 --fast --subset $subset $@
-python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR100 --pretrained_on CIFAR100 --fast --subset $subset $@
-python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset Core50 --pretrained_on ImageNet --architecture resnet --subset $subset --fast $@
-python main.py --scenario_name Domaingit --num_tasks 1 --name_algo baseline  --dataset Core10Lifelong --pretrained_on ImageNet --architecture resnet --subset $subset  --fast $@
+# here Core10Lifelong is disjoint because there is only one task
+python main.py --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset Core10Lifelong --pretrained_on ImageNet --architecture resnet  --fast $@
 
-done #list_subsets
