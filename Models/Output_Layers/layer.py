2f623efbeb5f30b7bbae7f18474677ff20444163
Thomas George <tfjgeorge@gmail.com>
2021-05-11 18:36:10 -0400

    Update layer.py
    change cosine layer initialization to kaiming normal

--- Models/Output_Layers/layer.py
+++ Models/Output_Layers/layer.py
@@ -10,28 +10,24 @@ import math
 
 
 class CosineLayer(nn.Module):
-    """ Custom Linear layer but mimics a standard linear layer """
-
     def __init__(self, size_in, size_out):
         super().__init__()
         self.size_in, self.size_out = size_in, size_out
         weight = torch.Tensor(size_out, size_in)
         self.weight = nn.Parameter(weight)  # nn.Parameter is a Tensor that's a module parameter.
 
-        # self.cos = nn.CosineSimilarity(dim=1, eps=1e-6)
-
-        # initialize weights and biases
-        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))  # weight init
-        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
-        self.bias = torch.zeros(size_out)
+        # initialize weights
+        nn.init.kaiming_normal_(self.weight)  # weight init
 
     def forward(self, x):
         cosine_out = []
+        cosine_out
         for i in range(self.size_out):
             cosine_out.append(torch.cosine_similarity(x, self.weight[i, :].unsqueeze(0)).unsqueeze(-1))
 
         x = torch.cat(cosine_out, dim=1)
         return x
+
 
 
 class MIMO(nn.Module):
