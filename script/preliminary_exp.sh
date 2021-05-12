94c898a5c9b90ef7b334a9673f023284c6d8cc0b
TLESORT <t.lesort@gmail.com>
2021-05-12 19:53:39 -0400

    fix variable name

--- script/preliminary_exp.sh
+++ script/preliminary_exp.sh
@@ -10,14 +10,13 @@ for seed in $seeds ;do
 for lr in $lrs ;do
 for head in $list_heads ;do
 sbatch script/run_on_cluster.sh --lr $lr --seed $seed --OutLayer $head
-#./script/test_run.sh $seed
 done #head
 done # lrs
 
 
-for head_masked_WO_lr in $list_heads_WO_lr ;do
-sbatch script/run_on_cluster.sh --seed $seed --OutLayer $head_masked_WO_lr
-done #head_masked
+for head_WO_lr in $list_heads_WO_lr ;do
+sbatch script/run_on_cluster.sh --seed $seed --OutLayer $head_WO_lr
+done #head_WO_lr
 
 done #seed
 
