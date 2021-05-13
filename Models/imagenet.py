fix group masking

Models/imagenet.py
@@ -88,5 +88,5 @@ class ImageNetModel(nn.Module):
     def update_head(self, epoch):
         self.get_last_layer().update(epoch)
 
-    def get_loss(self, out, labels, loss_func, masked=False):
+    def get_loss(self, out, labels, loss_func, masked=None):
         return self.head.get_loss(out, labels, loss_func, masked)

Models/model.py
@@ -49,5 +49,5 @@ class Model(nn.Module):
 
         return x
 
-    def get_loss(self, out, labels, loss_func, masked=False):
+    def get_loss(self, out, labels, loss_func, masked=None):
         return self.head.get_loss(out, labels, loss_func, masked)