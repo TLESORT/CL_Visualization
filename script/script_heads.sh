c98641a3feefca2942c080e161f80adc7296ddef
TLESORT <t.lesort@gmail.com>
2021-05-12 21:37:59 -0400

    add script for continual experiments

--- script/script_heads.sh
+++ script/script_heads.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-python main.py --scenario_name Disjoint --num_tasks 5 --name_algo baseline  --dataset CIFAR10 --pretrained_on CIFAR10 --fast $@
 python main.py --scenario_name Disjoint --num_tasks 5 --name_algo baseline  --dataset CIFAR10 --pretrained_on CIFAR100 --fast $@
-python main.py --scenario_name Disjoint --num_tasks 5 --name_algo baseline  --dataset CIFAR100 --pretrained_on CIFAR10 --fast $@
-python main.py --scenario_name Disjoint --num_tasks 5 --name_algo baseline  --dataset CIFAR100 --pretrained_on CIFAR100 --fast $@
+#python main.py --scenario_name Disjoint --num_tasks 5 --name_algo baseline  --dataset CIFAR100 --pretrained_on CIFAR10 --fast $@
 python main.py --scenario_name Disjoint --num_tasks 10 --name_algo baseline  --dataset Core50 --pretrained_on ImageNet --architecture resnet --fast $@
 python main.py --scenario_name Domain --num_tasks 8 --name_algo baseline  --dataset Core10Lifelong --pretrained_on ImageNet --architecture resnet  --fast $@
+
 
