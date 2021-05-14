aadda300969d89c36881b18f980df9b8b39b4680
TLESORT <t.lesort@gmail.com>
2021-05-14 09:28:14 -0400

    test 2 run

--- script/single_runs.sh
+++ script/single_runs.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 
-command="python main.py --scenario_name Disjoint --num_tasks 5 --name_algo baseline --dataset CIFAR10 --pretrained_on CIFAR100 --OutLayer Linear --seed 1 --fast"
+command="python main.py  --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset Core50 --pretrained_on ImageNet --fast --lr 0.1 --seed 1 --OutLayer Linear --subset 500 --architecture resnet"
 sbatch script/single_on_cluster.sh $command
 
-command="python main.py  --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR100 --pretrained_on CIFAR10 --fast --seed 0 --OutLayer CosLayer"
-sbatch script/single_on_cluster.sh $command
-
-command="python main.py  --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset CIFAR100 --pretrained_on CIFAR10 --fast --seed 1 --OutLayer Linear --masked_out"
+command="python main.py  --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset Core50 --pretrained_on ImageNet --fast --lr 0.1 --seed 1 --OutLayer Linear_no_bias --subset 200 --architecture resnet"
 sbatch script/single_on_cluster.sh $command
 
 
+command="python main.py  --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset Core50 --pretrained_on ImageNet --fast --lr 0.1 --seed 0 --OutLayer WeightNorm --subset 1000 --architecture resnet"
+sbatch script/single_on_cluster.sh $command
+
+
+command="python main.py  --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset Core10Lifelong --pretrained_on ImageNet --fast --lr 0.1 --seed 1 --OutLayer Linear --subset 500 --architecture resnet"
+sbatch script/single_on_cluster.sh $command
+
+
+command="python main.py  --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset Core10Lifelong --pretrained_on ImageNet --fast --lr 0.1 --seed 1 --OutLayer Linear_no_bias --subset 200 --architecture resnet"
+sbatch script/single_on_cluster.sh $command
+
+
+command="python main.py  --scenario_name Disjoint --num_tasks 1 --name_algo baseline  --dataset Core10Lifelong --pretrained_on ImageNet --fast --lr 0.1 --seed 0 --OutLayer WeightNorm --subset 1000 --architecture resnet"
+sbatch script/single_on_cluster.sh $command
