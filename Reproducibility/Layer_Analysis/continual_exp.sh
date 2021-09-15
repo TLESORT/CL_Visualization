b09eef2b9c58a00333e2638e40a9f2810ff81e26
TLESORT <t.lesort@gmail.com>
2021-09-15 20:01:49 +0200

    fix reproducibility scripts

--- Reproducibility/Layer_Analysis/continual_exp.sh
+++ Reproducibility/Layer_Analysis/continual_exp.sh
@@ -8,20 +8,20 @@ cd ../..
 for seed in $seeds ;do
 
 for head in list_heads_01 ;do
-sbatch script/script_continual.sh --lr 0.1 --seed $seed --OutLayer $head
-sbatch script/script_continual.sh --lr 0.1 --seed $seed --OutLayer $head_masked --masked_out single
-sbatch script/script_continual.sh --lr 0.1 --seed $seed --OutLayer $head_masked --masked_out group
+./script_continual.sh --lr 0.1 --seed $seed --OutLayer $head
+./script_continual.sh --lr 0.1 --seed $seed --OutLayer $head_masked --masked_out single
+./script_continual.sh --lr 0.1 --seed $seed --OutLayer $head_masked --masked_out group
 done #head
 
 for head in list_heads_001 ;do
-sbatch script/script_continual.sh --lr 0.01 --seed $seed --OutLayer $head
-sbatch script/script_continual.sh --lr 0.01 --seed $seed --OutLayer $head_masked --masked_out single
-sbatch script/script_continual.sh --lr 0.01 --seed $seed --OutLayer $head_masked --masked_out group
+./script_continual.sh --lr 0.01 --seed $seed --OutLayer $head
+./script_continual.sh --lr 0.01 --seed $seed --OutLayer $head_masked --masked_out single
+./script_continual.sh --lr 0.01 --seed $seed --OutLayer $head_masked --masked_out group
 done #head
 
 
 for head_WO_lr in $list_heads_WO_lr ;do
-sbatch script/script_continual.sh --seed $seed --OutLayer $head_WO_lr
+./script_continual.sh --seed $seed --OutLayer $head_WO_lr
 done #head_WO_lr
 
 done #seed
--- Reproducibility/Layer_Analysis/script_continual.sh
+++ Reproducibility/Layer_Analysis/script_continual.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ../..
+
 python main.py --scenario_name Disjoint --num_tasks 5 --name_algo baseline  --dataset CIFAR10 --pretrained_on CIFAR100 --fast $@
 #python main.py --scenario_name Disjoint --num_tasks 5 --name_algo baseline  --dataset CIFAR100 --pretrained_on CIFAR10 --fast $@
 python main.py --scenario_name Disjoint --num_tasks 10 --name_algo baseline  --dataset Core50 --pretrained_on ImageNet --architecture resnet --fast $@
