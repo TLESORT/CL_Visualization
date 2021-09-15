82f68ae95b5933d820abe9c2f3a5f82439e89864
TLESORT <t.lesort@gmail.com>
2021-09-15 20:23:41 +0200

    add lr loop into cl exps

--- script/continual_exp.sh
+++ script/continual_exp.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 seeds="0 1"
-list_heads=" Linear CosLayer Linear_no_bias WeightNorm OriginalWeightNorm"
+list_heads="Linear CosLayer Linear_no_bias WeightNorm OriginalWeightNorm"
 list_heads_WO_lr="SLDA MeanLayer MedianLayer KNN"
 
-lr=0.1
+lrs="0.1 0.01 0.001"
 
 for seed in $seeds ;do
+
+for lr in lrs ;do
 
 for head in $list_heads ;do
 sbatch script/run_on_cluster_continual.sh --lr $lr --seed $seed --OutLayer $head
@@ -14,6 +16,8 @@ sbatch script/run_on_cluster_continual.sh --lr $lr --seed $seed --OutLayer $head
 sbatch script/run_on_cluster_continual.sh --lr $lr --seed $seed --OutLayer $head_masked --masked_out group
 sbatch script/run_on_cluster_continual.sh --lr $lr --seed $seed --OutLayer $head_masked --masked_out MHead
 done #head
+
+done #lrs
 
 
 for head_WO_lr in $list_heads_WO_lr ;do
