b28dd09929f4ea2d9f5814585c502e432ffe265c
TLESORT <t.lesort@gmail.com>
2021-09-15 20:17:47 +0200

    update scripts and add finetuning scripts

--- script/continual_exp.sh
+++ script/continual_exp.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 seeds="0 1"
-list_heads=" Linear CosLayer Linear_no_bias WeightNorm"
+list_heads=" Linear CosLayer Linear_no_bias WeightNorm OriginalWeightNorm"
 list_heads_WO_lr="SLDA MeanLayer MedianLayer KNN"
+
+lr=0.1
 
 for seed in $seeds ;do
 
 for head in $list_heads ;do
-sbatch script/run_on_cluster_continual.sh --lr 0.1 --seed $seed --OutLayer $head
-sbatch script/run_on_cluster_continual.sh --lr 0.1 --seed $seed --OutLayer $head_masked --masked_out single
-sbatch script/run_on_cluster_continual.sh --lr 0.1 --seed $seed --OutLayer $head_masked --masked_out group
+sbatch script/run_on_cluster_continual.sh --lr $lr --seed $seed --OutLayer $head
+sbatch script/run_on_cluster_continual.sh --lr $lr --seed $seed --OutLayer $head_masked --masked_out single
+sbatch script/run_on_cluster_continual.sh --lr $lr --seed $seed --OutLayer $head_masked --masked_out group
 done #head
 
 
