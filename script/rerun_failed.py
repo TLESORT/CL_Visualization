10fc643c5ebb397a5a4dd525d0842d2efc288632
TLESORT <t.lesort@gmail.com>
2021-12-07 22:23:41 -0500

    modify code to not run several time the same command

--- script/rerun_failed.py
+++ script/rerun_failed.py
@@ -10,7 +10,8 @@ runs = api.runs(NAME_PROJECT)
 LIST_ARGUMENTS = ["seed", "scenario_name", "num_tasks", "dataset","name_algo", "lr", "OutLayer", "nb_samples_rehearsal_per_class", "spurious_corr"]
 # TO ADD STILL :  root_dir, data_dir
 
-commands = '#!/bin/bash\n\n'
+
+list_commands = []
 
 counter = 0
 for run in runs:
@@ -18,12 +19,21 @@ for run in runs:
 
         if run.config["seed"]==1664:
             continue
-
-        commands += "python main.py"
+        command = ''
         for argument in LIST_ARGUMENTS:
-            commands += f" --{argument} {run.config[argument]}"
+            command += f" --{argument} {run.config[argument]}"
 
-        commands += " --fast $@ \n\n"
+
+        list_commands.append(command)
+
+len_before = len(list_commands)
+print("remove duplicate entries")
+list_commands = list( dict.fromkeys(list_commands) )
+print(f"{len_before-len(list_commands)} duplicate entries removed")
+
+commands = '#!/bin/bash\n\n'
+for command in list_commands:
+    commands += f"python main.py {command}  --fast $@ \n\n"
 
 f = open("failed_runs.sh", "w")
 f.write(commands)
