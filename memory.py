e47428aee94e98a60ab3c5cfb0240b0a18e59c3b
TLESORT <t.lesort@gmail.com>
2022-04-28 13:49:38 -0400

    make memory compatible to  image path array

--- memory.py
+++ memory.py
@@ -21,8 +21,12 @@ class MemorySet(ArrayTaskSet):
         super().__init__(x=x, y=y, t=t, trsf=trsf, target_trsf=None)
 
         if isinstance(x[0], str):
+            print("OUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+            print(x[0].type())
             self.data_type = TaskType.IMAGE_PATH
         else:
+            print("NOOOOOOOOOOOOOOOOOOOOONNNNNNNNNNNNNNNNNNNNNNNNNN")
+            print(x[0].type())
             self.data_type = TaskType.IMAGE_ARRAY
 
         list_labels_id = range(len(self._y))
